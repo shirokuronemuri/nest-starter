46439aa409c592f239d6d11d04335f5fcda1d7ef
Ihor Pavliuk <akira.pavl@gmail.com>
2025-11-28 13:52:48 +0200

    starter project scaffolding, core module, prisma installed

---
+++ src/core/core.module.ts
@@ -0,0 +1,49 @@
+import {
+  Global,
+  Module,
+  type MiddlewareConsumer,
+  type NestModule,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import config from 'src/config';
+import { TransformResponseInterceptor } from './interceptors/transform-response/transform-response.interceptor';
+import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
+import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
+import { LoggerService } from './services/logger/logger.service';
+import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    LoggerService,
+  ],
+  exports: [LoggerService],
+})
+export class CoreModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
