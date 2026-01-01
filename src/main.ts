4c2a241753533fd6d5cbfb18f43f0fe1122ea337
Ihor Pavliuk <akira.pavl@gmail.com>
2026-01-01 23:16:14 +0200

    Add persisting swagger auth

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, cleanupOpenApiDoc(document));
+  SwaggerModule.setup('docs', app, cleanupOpenApiDoc(document), {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
