d7c34de23af95ec7286672949dbbbab9a5ed9487
Ihor Pavliuk <akira.pavl@gmail.com>
2026-01-12 19:56:18 +0200

    added helpers and default zod schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
-import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [CoreModule, TestModule],
+  imports: [CoreModule],
   controllers: [],
   providers: [],
 })
